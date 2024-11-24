cd2b8e921bc4689afc7e56ad2e58f89e725bc02c
Lux Aliaga <me@lux.rip>
2024-11-24 17:14:03 -0300

    flake: Inicializar
    Para poder trabajar en el proyecto dentro de Nix.

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            poetry
+            (python311.withPackages (python-pkgs: with python-pkgs; [
+              flask
+              huey
+            ]))
+          ];
+        };
+      }
+    );
+}
