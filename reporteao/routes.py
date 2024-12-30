e08745b30722ccf3bb157f92e07fcea015d73e2f
Lux Aliaga <lux.aliaga@usach.cl>
2024-12-30 15:53:45 -0300

    routes: Validar correo
    Verifica si el correo entregado está en minúsculas, y que solo contenga
    letras y puntos.

--- reporteao/routes.py
+++ reporteao/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session, render_template
+from flask import Blueprint, request, session, render_template, redirect
 from argon2 import PasswordHasher
 from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
 from .email import enviar_correo
@@ -66,9 +66,14 @@ def register():
     if request.method == 'POST':
         # Se consiguen los datos desde el formulario
         nombre = str(request.form['nombre'])
-        # TODO: Validar si el correo está bien escrito
-        email = str(request.form['email']) + '@usach.cl'
         clave = ph.hash(str(request.form['clave']))
+        email = str(request.form['email']) + '@usach.cl'
+
+        # Valida si el correo está bien escrito
+        for char in email:
+            if not ((char.isalpha() and char.islower()) or char == '.'):
+                return 'Correo no válido'
+        
 
         # Se invalida la solicitud si las contraseñas no son iguales
         if request.form['clave'] != request.form['clave2']:
@@ -88,10 +93,9 @@ def register():
 
         return "Se ha enviado un enlace de verificación a su correo institucional. Haga click en él para terminar de crear su cuenta."
     else:
-        # TODO: Agregar formulario de registro
         return render_template('register.html', title='Registrarse')
 
-@bp.route('/add')
+@bp.route('/add', methods=['POST', 'GET'])
 def agregar_reporte():
     return "TODO"
 
@@ -146,7 +150,7 @@ def verificar(id):
     
     if codigo[2] == 0:
         usuario = db.actualizar_nivel(codigo[0], 0)
-        return "Usuario activado"
+        return redirect('/', code=302)
     
     if codigo[2] == 1:
         # TODO: Agregar sistema de reestablecimiento de claves
